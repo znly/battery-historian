737f527639d37368d2c87ad61126abd5c33b2789
Clemence Saussez <clemence@zen.ly>
2016-10-26 17:36:06 +0200

    dockerfiles: adds missing dirs / packages

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
 FROM progrium/busybox
 
+RUN opkg-install python-light python-codecs
+
 COPY bin/battery-historian /data/battery-historian
 COPY compiled /data/compiled
 COPY templates /data/templates
 COPY static /data/static
+COPY scripts /data/scripts
+COPY third_party/flot-axislabels /data/third_party/flot-axislabels
 
 WORKDIR /data
 ENTRYPOINT "./battery-historian"
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --file Dockerfile.compile . -t battery-historian:compile
-docker run -v $(pwd)/bin:/output -v $(pwd)/compiled:/go/src/github.com/google/battery-historian/compiled battery-historian:compile
+docker run -v $(pwd)/bin:/output -v $(pwd)/compiled:/go/src/github.com/google/battery-historian/compiled -v $(pwd)/third_party:/go/src/github.com/google/battery-historian/third_party battery-historian:compile
 docker build . -t battery-historian:latest
